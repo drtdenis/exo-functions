1dcff6ff6fbc3e8286a1de9ce790d3a215e03168
drtdenis <drouetdenis2@gmail.com>
2021-03-08 00:22:50 +0100

    exo-functions

--- countWheels.js
+++ countWheels.js
@@ -1,3 +1,6 @@
-const countwheels = (nb-monocycle, nb_moto, nb_car, nb_limousine) => {
-  return nb_monocylce
+const countwheels = ( nb_monocylce, nb_moto, nb_car, nb_limousine) => {
+
+  return nb_monocylce * 1 + nb_moto * 2 + nb_car * 4 + nb_limousine * 6
 }
+      console.log(countwheels(10, 20, 7, 2))
+      console.log(countwheels(2, 3, 4, 5))
